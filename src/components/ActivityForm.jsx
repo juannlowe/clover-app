10d98a460c9872dbacde2dc77c0b688aed780c99
juannlowe <juanbranca16@gmail.com>
2024-01-12 13:45:02 -0800

    cleaning code

--- src/components/ActivityForm.jsx
+++ src/components/ActivityForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const ActivityForm = () => {
   const [fullName, setFullName] = useState('');
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import ActivityForm from './ActivityForm.jsx'
 
 const Body = () => {
   return (
     <main>
-      <h2>Activities info</h2>
       <ActivityForm/>
     </main>
   );
