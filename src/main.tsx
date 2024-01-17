6d7c1376e853727ab36510eacd0f6d0232ae9bc0
juannlowe <juanbranca16@gmail.com>
2024-01-17 11:29:07 -0800

    refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import SignIn from './components/googleSignIn/SignIn.jsx'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
