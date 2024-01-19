1cbb6c621fbb627495acac6e10fa50ea27e3cd60
juannlowe <juanbranca16@gmail.com>
2024-01-19 11:51:34 -0800

    fixed log in info

--- src/components/googleSignIn/SignIn.tsx
+++ src/components/googleSignIn/SignIn.tsx
@@ -3,22 +3,26 @@ import { auth, provider } from "./config.ts";
 import {signInWithPopup} from "firebase/auth";
 import { useEffect } from "react";
 import { Button } from '@mui/material'
+import * as R from 'ramda'
+
+const getFirsName = R.pipe(R.split(" "),R.head)
 
 function SignIn(){
     const [value,setValue] = useState('')
     const handleClick = ()=>{
         signInWithPopup(auth,provider).then((data)=>{
-            setValue(data.user.email as string)
-            localStorage.setItem("email", data.user.email as string)
+          const firstName = getFirsName(data.user.displayName)
+            setValue(firstName as string)
+            localStorage.setItem("displayName", firstName as string)
         })
     }
 
     useEffect(()=>{
-        setValue(localStorage.getItem('email') as string)
+        setValue(localStorage.getItem('displayName') as string)
     },[])
     return (
         <div>
-          {value ? "Welcome Back!" : <Button
+          {value ? `Welcome Back ${value}!` : <Button
             onClick={handleClick}
             variant="contained"
             sx={{ backgroundColor: 'white', color: 'black' }}
