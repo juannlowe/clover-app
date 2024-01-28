b601a736ff7abe172e1bb9ba0a7905b0d01c1f8a
juannlowe <juanbranca16@gmail.com>
2024-01-28 13:31:40 -0800

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,19 @@ const Footer = () => {
     <footer
       style={{
         display: 'flex',
+        flexDirection: 'column',
         backgroundColor: 'black',
         color: 'white',
         justifyContent: 'center',
+        alignItems: 'center',
         position: 'fixed',
         bottom: 0,
         width: '100%',
+        padding: '10px',
       }}
     >
-      <p>&copy; {new Date().getFullYear()} All Rights Reserved Clover / Join cool activities!</p>
+        <p style={{margin: '0'}}>Join cool activities!</p>
+        <p>&copy; {new Date().getFullYear()} All Rights Reserved Clover</p>
     </footer>
   );
 };
