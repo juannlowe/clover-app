d7565254f6e38f9a6349ed96056da278e418bcef
juannlowe <juanbranca16@gmail.com>
2024-02-03 19:51:00 -0800

    working on about us

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+// AboutUsPage.js
+import React from 'react';
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the About Us page content. Add any relevant information about your organization or company here.</p>
+    </div>
+  );
+};
+
+export default AboutUsPage;
